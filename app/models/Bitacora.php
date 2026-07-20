60851d443072a693056e712d6d04cf6978293ea3
iAmDereck <141465076+iAmDereckV@users.noreply.github.com>
2026-07-20 13:57:45 -0600

    Update Bitacora.php

--- app/models/Bitacora.php
+++ app/models/Bitacora.php
@@ -1,9 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../config/database.php';
-
-
-
 class Bitacora
 {
     private $conexion;
@@ -14,36 +11,34 @@ class Bitacora
     }
     public function listar()
     {
-        $sql = "SELECT
-            v.fecha,'VENTA' tipo,CONCAT('Factura #',v.id) referencia,
-            CONCAT('Venta a ',c.nombres) descripcion,v.total entrada,
-            0 salida,u.nombre usuario
-        FROM ventas v
-        INNER JOIN clientes c ON c.id = v.cliente_id
-        INNER JOIN usuarios u ON u.id = v.usuario_id
-        WHERE v.estado = 'COMPLETADA'
-        UNION ALL
-        SELECT
-            c.fecha,'COMPRA',CONCAT('Compra #',c.id),
-            CONCAT('Compra a ',p.nombre),c.total,0,u.nombre
-        FROM compras c
-        INNER JOIN proveedores p ON p.id = c.proveedor_id
-        INNER JOIN usuarios u ON u.id = c.usuario_id
-        UNION ALL
-        SELECT
-            g.fecha,'GASTO',CONCAT('Gasto #',g.id),g.descripcion,
-            0,g.monto,u.nombre
-        FROM gastos g
-        INNER JOIN usuarios u ON u.id = g.usuario_id
-        UNION ALL
-        SELECT
-            v.fecha,'ANULACION',CONCAT('Factura #',v.id),
-            CONCAT('Venta anulada de ',c.nombres),0,v.total,u.nombre
-        FROM ventas v
-        INNER JOIN clientes c ON c.id = v.cliente_id
-        INNER JOIN usuarios u ON u.id = v.usuario_id
-        WHERE v.estado = 'ANULADA'
-        ORDER BY fecha DESC
+        $sql = "SELECT v.fecha,'VENTA' tipo,CONCAT('Factura #', v.id) referencia,CONCAT('Venta a ', c.nombres) descripcion,
+            v.total entrada,0 salida,u.nombre usuario
+            FROM ventas v
+            INNER JOIN clientes c ON c.id = v.cliente_id
+            INNER JOIN usuarios u ON u.id = v.usuario_id
+            WHERE v.estado = 'COMPLETADA'
+            UNION ALL
+            SELECT c.fecha, 'COMPRA', CONCAT('Compra #', c.id), CONCAT('Compra a ', p.nombre), 0, c.total, u.nombre
+            FROM compras c
+            INNER JOIN proveedores p ON p.id = c.proveedor_id
+            INNER JOIN usuarios u ON u.id = c.usuario_id
+            UNION ALL
+            SELECT g.fecha, 'GASTO', CONCAT('Gasto #', g.id), g.descripcion, 0, g.monto, u.nombre
+            FROM gastos g
+            INNER JOIN usuarios u ON u.id = g.usuario_id
+            UNION ALL
+            SELECT v.fecha, 'ANULACION', CONCAT('Factura #', v.id), CONCAT('Venta anulada de ', c.nombres),  v.total,0, u.nombre
+            FROM ventas v
+            INNER JOIN clientes c ON c.id = v.cliente_id
+            INNER JOIN usuarios u ON u.id = v.usuario_id
+            WHERE v.estado = 'ANULADA'
+            UNION ALL
+            SELECT c.fecha, 'ANULACION', CONCAT('COMPRA #', c.id), CONCAT('Compra anulada de ', p.nombre), 0, c.total, u.nombre
+            FROM compras c
+            INNER JOIN proveedores p ON p.id = c.proveedor_id
+            INNER JOIN usuarios u ON u.id = c.usuario_id
+            WHERE c.estado = 'ANULADA'
+            ORDER BY fecha DESC;
         ";
         $stmt = $this->conexion->prepare($sql);
         $stmt->execute();
